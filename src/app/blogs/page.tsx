ff739a53a44348abff53688431cbe898d7886d66
Saurabh <saurabh@millicent.in>
2024-07-25 15:57:40 +0530

    feature : 1. blog card animation added

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Heading from "@/components/heading/Heading";
+import "./blog.css";
 
 const Blog = () => {
   const [loadSwiper, setLoadSwiper] = useState(false);
@@ -13,128 +14,128 @@ const Blog = () => {
   const data = [
     {
       id: 1,
-      image: "https://picsum.photos/1100/1100",
+      image: new URL("https://picsum.photos/1100/1100"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-August-2023",
     },
     {
       id: 2,
-      image: "https://picsum.photos/1200/1200",
+      image: new URL("https://picsum.photos/1200/1200"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-May-2024",
     },
     {
       id: 3,
-      image: "https://picsum.photos/1300/1300",
+      image: new URL("https://picsum.photos/1300/1300"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
-      description: "Best Construction Quality Award - Reality Quaters 2023",
+      description: "Best Construction Quality - Reality Quaters 2023",
       date: "17-June-2024",
     },
     {
       id: 4,
-      image: "https://picsum.photos/1000/1000",
+      image: new URL("https://picsum.photos/1000/1000"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-July-2024",
     },
     {
       id: 5,
-      image: "https://picsum.photos/1400/1400",
+      image: new URL("https://picsum.photos/1400/1400"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-January-2024",
     },
     {
       id: 6,
-      image: "https://picsum.photos/1500/1500",
+      image: new URL("https://picsum.photos/1500/1500"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
-      description: "Best Construction Quality Award - Reality Quaters 2023",
+      description: "Best Construction Quality - Reality Quaters 2023",
       date: "17-February-2024",
     },
     {
       id: 7,
-      image: "https://picsum.photos/1600/1600",
+      image: new URL("https://picsum.photos/1600/1600"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-May-2024",
     },
     {
       id: 8,
-      image: "https://picsum.photos/1700/1700",
+      image: new URL("https://picsum.photos/1700/1700"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-June-2024",
     },
     {
       id: 9,
-      image: "https://picsum.photos/1800/1800",
+      image: new URL("https://picsum.photos/1800/1800"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
-      description: "Best Construction Quality Award - Reality Quaters 2023",
+      description: "Best Construction Quality - Reality Quaters 2023",
       date: "17-July-2024",
     },
     {
       id: 10,
-      image: "https://picsum.photos/1900/1900",
+      image: new URL("https://picsum.photos/1900/1900"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-August-2023",
     },
     {
       id: 11,
-      image: "https://picsum.photos/1200/1200",
+      image: new URL("https://picsum.photos/1200/1200"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-May-2024",
     },
     {
       id: 12,
-      image: "https://picsum.photos/1300/1300",
+      image: new URL("https://picsum.photos/1300/1300"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
-      description: "Best Construction Quality Award - Reality Quaters 2023",
+      description: "Best Construction Quality - Reality Quaters 2023",
       date: "17-June-2024",
     },
     {
       id: 13,
-      image: "https://picsum.photos/1000/1000",
+      image: new URL("https://picsum.photos/1000/1000"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-July-2024",
     },
     {
       id: 14,
-      image: "https://picsum.photos/1400/1400",
+      image: new URL("https://picsum.photos/1400/1400"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-January-2024",
     },
     {
       id: 15,
-      image: "https://picsum.photos/1500/1500",
+      image: new URL("https://picsum.photos/1500/1500"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
-      description: "Best Construction Quality Award - Reality Quaters 2023",
+      description: "Best Construction Quality - Reality Quaters 2023",
       date: "17-February-2024",
     },
     {
       id: 16,
-      image: "https://picsum.photos/1600/1600",
+      image: new URL("https://picsum.photos/1600/1600"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-May-2024",
     },
     {
       id: 17,
-      image: "https://picsum.photos/1700/1700",
+      image: new URL("https://picsum.photos/1700/1700"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
       description: "Top Emerging Developer - Times Of India 2023",
       date: "17-June-2024",
     },
     {
       id: 18,
-      image: "https://picsum.photos/1800/1800",
+      image: new URL("https://picsum.photos/1800/1800"),
       heading: "Reasons fueling the rising demand for luxury flats in Mumbai",
-      description: "Best Construction Quality Award - Reality Quaters 2023",
+      description: "Best Construction Quality - Reality Quaters 2023",
       date: "17-July-2024",
     },
   ];
@@ -163,20 +164,17 @@ const Blog = () => {
                       key={item.id}
                       className="col-lg-4 col-md-6 col-sm-6 mb-30"
                     >
-                      <div className="blog-item">
-                        <div className="blog-img">
-                          <img src={item.image} alt={item.description} />
-                        </div>
-                        <div className="blog-content">
-                          <h5 className="blog-title fz-20 fw-bold mb-0 text-grey">
-                            {/* <Link href="/dark/blog-post" className="text-grey">{item.heading}</Link> */}
+                      <div
+                        className="blog-card"
+                        style={{ backgroundImage: `url(${item.image})` }}
+                      >
+                        <div className="blog-card-content">
+                          <h5 className="blog-card-title flex-wrap main-color4">
                             {item.heading}
                           </h5>
-                          <p className="blog-date fz-14">{item.date}</p>
-                          <h5 className="blog-title fz-16 mb-2">
-                            {/* <Link href="/dark/blog-post">{item.description}</Link> */}
+                          <div className="blogcard-body mb-2">
                             {item.description}
-                          </h5>
+                          </div>
                           <Link
                             href="/dark/blog-post"
                             className="btn main-colorbg3 text-light"
