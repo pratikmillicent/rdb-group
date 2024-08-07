11fd96b875674bed478ffb575fae52a2bddb00f1
Rajnish Yadav <rajnishyadav5509@gmail.com>
2024-08-07 18:52:39 +0530

    changes 1. about page video responsiv

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,22 +12,22 @@ import DirectorCard from "./components/DirectorCard";
 function About() {
   return (
     <>
-      <main className="mw-100 ">
-        <div>
-          <video
-            className="mw-100 w-100"
-            style={{ height: "calc(100dvh - 80px)", objectFit: "initial" }}
-            src="/assets/video/AboutHero.mp4"
-            autoPlay
-            muted
-            playsInline
-            loop
-            preload="auto"
-          ></video>
+      <main className="mw-100">
+        <div className="about-banner">
+          <div className="video-container">
+            <video
+              className="responsive-video"
+              src="/assets/video/AboutHero.mp4"
+              autoPlay
+              muted
+              playsInline
+              loop
+              preload="auto"
+            ></video>
+          </div>
         </div>
 
         <VisionMission />
-
         <DirectorCard />
         <Team />
         {/* <TeamSection /> */}
