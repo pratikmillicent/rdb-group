e83ea79937e3aada114459db93c2f2e2ac261eb1
Abu <abu.h@millicent.in>
2024-09-02 16:32:04 +0530

    Fixed: 1. Added few changes in new group section

--- src/app/(home)/components/NewGroup.tsx
+++ src/app/(home)/components/NewGroup.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import loadBackgroudImages from "@/common/loadBackgroudImages";
 import isInView from "@/common/isInView";
@@ -187,76 +187,77 @@ const NewGroupGrid: React.FC = () => {
                 </div>
               </div>
               <div className="col-lg-6 sub-bg right">
-                {data?.map((item, index) => (
-                  <div
-                    className={`cont ${index === 0 ? "active" : ""}`}
-                    data-tab={`tab-${index + 1}`}
-                    key={index}
-                  >
-                    <div className="img-hiden">
-                      <Image
-                        src={item.image}
-                        alt="Landscape picture"
-                        width={800}
-                        height={500}
-                      />
-                    </div>
+                {data?.map((item, index) => {
+                  const isLastTwo = index >= data.length - 2;
+                  const marginStyle = isLastTwo
+                    ? { marginTop: "150px", marginBottom: "150px" }
+                    : { marginBottom: "250px" };
 
-                    <div className="d-flex justify-content-left align-items-left gap-3  pt-2">
-                      <div>
-                        <Heading
-                          headTitle={`R.D.Brothers ${item.type}`}
-                          isMargin=""
-                          textAlign="center"
+                  return (
+                    <div
+                      className={`cont ${index === 0 ? "active" : ""}`}
+                      data-tab={`tab-${index + 1}`}
+                      key={index}
+                      style={marginStyle}
+                    >
+                      <div className="img-hiden">
+                        <Image
+                          src={item.image}
+                          alt="Landscape picture"
+                          width={800}
+                          height={500}
                         />
                       </div>
 
-                      <div className="mt-5">
-                        <FaBuilding fontSize={28} color="var(--golden)" />
+                      <div className="d-flex justify-content-left align-items-end gap-3  pt-2">
+                        <div>
+                          <h5 className="director-title mb-0">R.D.Brothers</h5>
+                          <div className="d-flex align-items-center gap-3 mb-4 ">
+                            <h1 className="director-head  m-0">{item.type}</h1>
+                            <FaBuilding fontSize={24} color="var(--golden)" />
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                    {/* <h2 className="mb-15 text-grey">{item.title}.</h2> */}
-                    <div className="row">
-                      <div
-                        className="col-md-12 pb-4"
-                        // style={{ lineHeight: "2.3" }}
-                      >
-                        <p
-                          style={{
-                            textAlign: "justify",
-                          }}
-                          className="text-black fz-16 fw-400"
-                        >
-                          {item.text}.
-                        </p>
-                        <div
-                          className="vew-all mt-20 ml-25"
-                          style={{ marginBottom: "16px" }}
-                        >
-                          <Link href="#">
-                            <span className="text-black fz-17 fw-400">
-                              Explore More
-                              <span>
-                                <svg
-                                  width="18"
-                                  height="18"
-                                  viewBox="0 0 18 18"
-                                  fill="none"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                >
-                                  <path
-                                    d="M13.922 4.5V11.8125C13.922 11.9244 13.8776 12.0317 13.7985 12.1108C13.7193 12.1899 13.612 12.2344 13.5002 12.2344C13.3883 12.2344 13.281 12.1899 13.2018 12.1108C13.1227 12.0317 13.0783 11.9244 13.0783 11.8125V5.51953L4.79547 13.7953C4.71715 13.8736 4.61092 13.9176 4.50015 13.9176C4.38939 13.9176 4.28316 13.8736 4.20484 13.7953C4.12652 13.717 4.08252 13.6108 4.08252 13.5C4.08252 13.3892 4.12652 13.283 4.20484 13.2047L12.4806 4.92188H6.18765C6.07577 4.92188 5.96846 4.87743 5.88934 4.79831C5.81023 4.71919 5.76578 4.61189 5.76578 4.5C5.76578 4.38811 5.81023 4.28081 5.88934 4.20169C5.96846 4.12257 6.07577 4.07813 6.18765 4.07812H13.5002C13.612 4.07813 13.7193 4.12257 13.7985 4.20169C13.8776 4.28081 13.922 4.38811 13.922 4.5Z"
-                                    fill="currentColor"
-                                  ></path>
-                                </svg>
+
+                      <div className="row">
+                        <div className="col-md-12 pb-4">
+                          <p
+                            style={{
+                              textAlign: "justify",
+                            }}
+                            className="text-black fz-16 fw-400"
+                          >
+                            {item.text}.
+                          </p>
+                          <div
+                            className="vew-all mt-20 ml-25"
+                            style={{ marginBottom: "16px" }}
+                          >
+                            <Link href="#">
+                              <span className="text-black fz-17 fw-400">
+                                Explore More
+                                <span>
+                                  <svg
+                                    width="18"
+                                    height="18"
+                                    viewBox="0 0 18 18"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M13.922 4.5V11.8125C13.922 11.9244 13.8776 12.0317 13.7985 12.1108C13.7193 12.1899 13.612 12.2344 13.5002 12.2344C13.3883 12.2344 13.281 12.1899 13.2018 12.1108C13.1227 12.0317 13.0783 11.9244 13.0783 11.8125V5.51953L4.79547 13.7953C4.71715 13.8736 4.61092 13.9176 4.50015 13.9176C4.38939 13.9176 4.28316 13.8736 4.20484 13.7953C4.12652 13.717 4.08252 13.6108 4.08252 13.5C4.08252 13.3892 4.12652 13.283 4.20484 13.2047L12.4806 4.92188H6.18765C6.07577 4.92188 5.96846 4.87743 5.88934 4.79831C5.81023 4.71919 5.76578 4.61189 5.76578 4.5C5.76578 4.38811 5.81023 4.28081 5.88934 4.20169C5.96846 4.12257 6.07577 4.07813 6.18765 4.07812H13.5002C13.612 4.07813 13.7193 4.12257 13.7985 4.20169C13.8776 4.28081 13.922 4.38811 13.922 4.5Z"
+                                      fill="currentColor"
+                                    ></path>
+                                  </svg>
+                                </span>
                               </span>
-                            </span>
-                          </Link>
+                            </Link>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  );
+                })}
               </div>
             </div>
           </div>
