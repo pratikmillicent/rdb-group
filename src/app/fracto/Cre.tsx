6576faab30414a5a8bdbe0b9676b5ab5b58d4eb6
Rajnish Yadav <rajnishyadav5509@gmail.com>
2024-09-04 20:01:08 +0530

    Feature: add cre section in movie

--- src/app/fracto/Cre.tsx
+++ src/app/fracto/Cre.tsx
@@ -1,77 +1,157 @@
-// Cre.js
 import React, { useState } from "react";
 import "./Fracto.css"; // Ensure this CSS file is correctly imported
 import Heading from "@/components/heading/Heading";
-import { FaPlus } from "react-icons/fa";
-import { FiMinus } from "react-icons/fi";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import { MdKeyboardArrowUp } from "react-icons/md";
-
-const AccordionItem = ({ id, title, content, isOpen, onClick }) => {
-  return (
-    <div
-      className={isOpen ? "border-0 " : "border rounded"}
-      style={{ marginBottom: "15px" }}
-    >
-      <div
-        className={`accordion-header ${
-          isOpen ? "bg-golden text-white rounded" : ""
-        }`}
-        onClick={() => onClick(id)}
-      >
-        <h5 className="accordion-title">{title}</h5>
-        <span className="accordion-toggle">
-          {isOpen ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
-        </span>
-      </div>
-
-      <div className={`accordionBody ${isOpen ? "open" : "closed"}`}>
-        <p>{content}</p>
-      </div>
-    </div>
-  );
-};
 
 const Cre = () => {
-  const [openId, setOpenId] = useState(1);
-
-  const handleToggle = id => {
-    setOpenId(openId === id ? null : id);
-  };
-
   return (
-    <div className=" container">
+    <div>
       <Heading headTitle="CRE" />
-      {/* <div className="accordion ">
-        <AccordionItem
-          id="1"
-          title="High Return on Investment"
-          content="Commercial properties often offer higher rental yields compared to residential properties. This is due to the long-term leases and typically higher rents associated with commercial real estate."
-          isOpen={openId === 1}
-          onClick={handleToggle}
-        />
-        <AccordionItem
-          id="2"
-          title="Stability and Reliability"
-          content="Commercial leases are often longer-term than residential leases, providing more stability and less frequent turnover. Additionally, businesses generally have more financial stability than individual tenants."
-          isOpen={openId === 2}
-          onClick={handleToggle}
-        />
-        <AccordionItem
-          id="3"
-          title="Diversification"
-          content="Investing in commercial real estate allows for diversification of your investment portfolio. Different types of commercial properties (e.g., retail, office, industrial) offer various risk and return profiles."
-          isOpen={openId === 3}
-          onClick={handleToggle}
-        />
-        <AccordionItem
-          id="4"
-          title="Tax Benefits"
-          content="Commercial real estate investments can offer various tax benefits, including depreciation deductions, mortgage interest deductions, and potential tax credits, which can enhance overall returns."
-          isOpen={openId === 4}
-          onClick={handleToggle}
-        />
-      </div> */}
+
+      <div
+        className="row position-relative"
+        style={{
+          background: "var(--background)",
+          paddingTop: "40px",
+          paddingBottom: "40px",
+        }}
+      >
+        <div className="w-100 mb-5 pb-5">
+          <div className="d-flex justify-content-center  align-items-start">
+            <h1
+              className="pb-5"
+              style={{
+                flex: 1,
+                fontSize: "56px",
+                fontWeight: 300,
+                lineHeight: "67px",
+
+                marginBottom: "10px",
+                paddingLeft: "5%",
+                color: "var(--navy)",
+              }}
+            >
+              Commercial <br />{" "}
+              <span
+                style={{
+                  color: "var(--golden)",
+                }}
+              >
+                {" "}
+                Real Estate
+              </span>
+            </h1>
+            <h6
+              style={{
+                paddingLeft: "5%",
+                flex: 1,
+                fontSize: "18px",
+                fontWeight: 400,
+                paddingRight: "5%",
+              }}
+            >
+              Rd brothers provides a wide selection of financing options for
+              commercial real estate businesses of all sizes
+            </h6>
+          </div>
+        </div>
+        <div className="col-md-5">
+          <img
+            src="https://www.jpmorgan.com/content/dam/jpmorgan/images/cb/commercial-real-estate/cb-cre-banner-commercial-property-financing-solutions-490x870-vertical.jpg"
+            alt="Commercial Property Financing Solutions"
+            className="img-fluid"
+          />
+        </div>
+        {/* <div
+          className="position-absolute"
+          style={{
+            left: "50%",
+            top: "5%",
+            fontSize: "56px",
+            fontWeight: 300,
+            lineHeight: "67px",
+          }}
+        >
+          Commercial <br /> Real Estate
+        </div> */}
+        <div
+          className="col-md-8 d-flex flex-wrap position-absolute "
+          style={{
+            right: "5%",
+            top: "20%",
+          }}
+        >
+          <div className="col-md-4 text-navy p-3 bg-transparent">
+            <div
+              className=" p-4 h-100 bg-white"
+              style={{
+                borderBottom: "5px solid var(--golden)",
+              }}
+            >
+              <h5>Term loans</h5>
+              <p>
+                Experience a straightforward process and exceptional customer
+                service when financing multifamily and other commercial
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4  text-navy p-3 bg-transparent">
+            <div
+              className=" p-4 h-100 bg-white"
+              style={{
+                borderBottom: "5px solid var(--golden)",
+              }}
+            >
+              <h5>Subscription lending</h5>
+              <p>
+                JPMorgan Chase commercial real estate and subscription finance
+                specialists work side-by-side with our clients to provide credit
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4  text-navy p-3 bg-transparent">
+            <div
+              className=" p-4 h-100 bg-white"
+              style={{
+                borderBottom: "5px solid var(--golden)",
+              }}
+            >
+              <h5>Refinancing</h5>
+              <p>
+                Whether it’s industrial, mixed-use, multifamily, office, or
+                retail property, we offer refinancing options through fixed- and
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4  text-navy p-3 bg-transparent">
+            <div
+              className=" p-4 h-100 bg-white"
+              style={{
+                borderBottom: "5px solid var(--golden)",
+              }}
+            >
+              <h5>Syndicated financing</h5>
+              <p>
+                Our team has extensive knowledge and experience providing
+                syndicated loans for commercial real estate, including
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4  text-navy p-3 bg-transparent">
+            <div
+              className=" p-4 h-100 bg-white"
+              style={{
+                borderBottom: "5px solid var(--golden)",
+              }}
+            >
+              <h5>Construction loans</h5>
+              <p>
+                We have short-term construction loans, construction-to-permanent
+                loans, and other commercial property financing available to fit
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
