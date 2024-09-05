ed644131b65a6ecefdc35355fc6073a747b8f31b
Rajnish Yadav <rajnishyadav5509@gmail.com>
2024-09-05 12:59:05 +0530

    fixes: 1. responsive award section in movie 2.  responsive cre section in factro

--- src/app/movies/AwardMovie.tsx
+++ src/app/movies/AwardMovie.tsx
@@ -33,15 +33,15 @@ const AwardMovie = () => {
   ];
 
   return (
-    <div className="">
+    <div className="container-fluid">
       <Heading headTitle="Awards" />
       <div className="row">
-        <div className="col-5">
+        <div className="col-md-5 col-12 mb-4">
           <div className="fz-18 text-golden">Awards Winning</div>
           <div
-            className=""
+            className="awards-heading"
             style={{
-              fontSize: "65px",
+              fontSize: "clamp(40px, 5vw, 65px)",
               fontWeight: 500,
               lineHeight: "1.3em",
               letterSpacing: "0px",
@@ -52,47 +52,19 @@ const AwardMovie = () => {
             Our Awards and Recognitions
           </div>
         </div>
-        <div className="col-7">
+        <div className="col-md-7 col-12">
           {awardsData.map((item, index) => (
             <div
               className="award-item d-flex align-items-start justify-content-between gap-3 p-3 border-bottom"
               key={index}
             >
               <div className="d-flex align-items-start gap-3">
-                <div
-                  className="award-id"
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: 500,
-                    lineHeight: "1.7em",
-                    color: "var(--golden)",
-                  }}
-                >
-                  ({item.id})
-                </div>
-                <div
-                  className="award-title"
-                  style={{
-                    fontSize: "30px",
-                    fontWeight: 500,
-                    lineHeight: "1.4em",
-                    letterSpacing: "0px",
-                    textTransform: "uppercase",
-                  }}
-                >
+                <div className="award-id text-golden fz-16">({item.id})</div>
+                <div className="award-title text-uppercase fz-30 fw-500">
                   {item.title}
                 </div>
               </div>
-              <div
-                className="award-year"
-                style={{
-                  fontSize: "30px",
-                  fontWeight: 500,
-                  lineHeight: "1.4em",
-                  letterSpacing: "0px",
-                  textTransform: "uppercase",
-                }}
-              >
+              <div className="award-year text-uppercase fz-30 fw-500">
                 {item.year}
               </div>
             </div>
